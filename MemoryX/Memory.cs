ba09e54fa681bd20407dddcd31d6ef9f0164086e
Ayuth Mangmesap <ayuth.mang@gmail.com>
2017-03-06 01:08:27 +0700

    added memory.cs

---
+++ MemoryX/Memory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryX
+{
+    public class Memory
+    {
+       public static void WriteMemoryInt()
+        {
+            Console.WriteLine("HEllo Wrold'");
+        }
+    }
+}
