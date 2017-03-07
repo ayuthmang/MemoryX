fa5eaea2e15faa9ced193c3b274a6db44ca9f127
bay <blacksource@windowslive.com>
2017-03-08 00:57:39 +0700

    Fixed WriteInt method

--- MemoryXTest/Program.cs
+++ MemoryXTest/Program.cs
@@ -14,20 +14,28 @@ namespace MemoryXTest
         public static extern IntPtr OpenProcess(int dwDesiredAccess,  bool bInheritHandle, int dwProcessId);
 
         [DllImport("kernel32.dll", SetLastError = true)]
-        static extern bool WriteProcessMemory(int hProcess, long lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
+        static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
+
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress, int value, int dwSize, ref int lpNumberOfBytesWritten);
 
         const int PROCESS_VM_WRITE = 0x0020;
         const int PROCESS_VM_OPERATION = 0x0008;
         static void Main(string[] args)
         {
             MemoryX.Memory myProc = new MemoryX.Memory();
-            myProc.getProcessHandle("notepad");
-            myProc.WriteInt(0x01666C68, 999);
-            //MemoryX.Memory.WriteMemoryInt();
-            //MemoryX.Memory.OpenProcess((int)MemoryX.Memory.ProcessAccess.AllAccess, false,999);
-            //Console.ReadLine();
+            myProc.getProcessHandle("Tutorial-x86_64");
+
+            //Console.WriteLine(myProc.getProcessID());
+            Console.WriteLine(myProc.WriteInt(0x014FDAD0, 1231235));
+
+            myProc.closeHandle();
             Console.Read();
-            //Process process = Process.GetProcessesByName("notepad")[0];
+
+
+
+            //Process process = Process.GetProcessesByName("Tutorial-i386")[0];
             //IntPtr processHandle = OpenProcess(0x1F0FFF, false, process.Id);
 
             //int bytesWritten = 0;
@@ -36,10 +44,10 @@ namespace MemoryXTest
             ////00000175DF9B5C70  47 00 6F 00 6F 00 64 00 20 00 42 00 79 00 65 00  G.o.o.d. .B.y.e.  
 
             //// replace 0x0046A3B8 with your address
-            //WriteProcessMemory((int)processHandle, 0x00000175DF9F20B0, buffer, buffer.Length, ref bytesWritten);
+            //WriteProcessMemory((int)processHandle, 0x01666C68, buffer, buffer.Length, ref bytesWritten);
 
             //Console.ReadLine();
-        
+
         }
     }
 }
